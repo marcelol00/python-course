539fe80f0c8ea30fe167bb6f7226bc583e13e9bc
Marcelo Almeida <marcelox.alm@gmail.com>
2019-12-03 19:34:20 -0300

    String manipulation
    single and double quotes strings, comments, paragraphs with triple single quotes, escape slash

---
+++ 2.1/21.py
@@ -0,0 +1,34 @@
+#!C:\Python34
+
+single_quote_str = 'single quotes'
+double_quote_str = "double quotes"
+
+sentence1 = 'I "really" like chocolate'
+sentence2 = "I 'really' like chocolate"
+
+print(sentence1)
+print(sentence2)
+
+sentence3 = 'I \'really\' like chocolate'
+sentence4 = "I \"really\" like chocolate"
+
+print(sentence3)
+print(sentence4)
+
+paragraph = '''
+This is a 
+long 
+paragraph
+'''
+
+print(paragraph)
+
+'''
+
+This is a 
+paragrapg
+commment
+
+'''
+
+#this is a single line comment
