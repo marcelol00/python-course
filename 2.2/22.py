70b3a378d86bff4977e895fd61212bb1dcc46e7c
Marcelo Almeida <marcelox.alm@gmail.com>
2019-12-03 19:47:15 -0300

    String manipulation
    using methods .upper, .capitalize, .tittle, .lower, .strip, and mixing them like .strip().upper()

---
+++ 2.2/22.py
@@ -0,0 +1,22 @@
+#!C:\Python34
+
+science = "SCIENCE"
+print(science)
+
+apple = "apple"
+print(apple)
+
+apple = apple.upper()
+print("upper", apple)
+
+apple = apple.capitalize()
+print("capitalize", apple)
+
+print("title", science.title())
+print("lower", science.lower())
+
+apple = "  apple  "
+print(apple)
+
+apple = apple.strip().upper()
+print("strip", apple)
